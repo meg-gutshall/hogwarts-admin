33c83100d14f938d157decb54bd12b0f4995e950
Meg Gutshall <meghan.gutshall@gmail.com>
2019-05-03 11:23:39 -0400

    Nest routes, edit resources, and finish wishlist

--- config/routes.rb
+++ config/routes.rb
@@ -4,13 +4,17 @@ Rails.application.routes.draw do
   get '/', to: 'welcome#home'
   post '/cast-spell', to: 'welcome#cast_spell'
 
-  resources :teachers, only: [:index, :show]
-  resources :students, only: [:index, :show]
+  resources :teachers, only: [:index]
+  resources :students, only: [:index]
   resources :subjects, only: [:index, :show]
-  resources :houses, only: [:index, :show]
+  resources :houses, only: [:index, :show] do
+    resources :students, only: [:index, :show]
+    resources :teachers, only: [:index, :show]
+  end
 
+  
   # Wishlist
-  # /teachers...
+  # /teachers => shows all teachers with link to their show pages
   # /students => shows all students with link to their show pages
   # /houses => shows all houses with links to their show pages
   # /houses/1 => shows first house with links to house's teachers and students show pages
